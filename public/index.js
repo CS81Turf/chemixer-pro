f52fed51d8e7203b580fbed35892def0d2964ab8
Chris <chrishabich3@gmail.com>
2025-10-29 10:37:25 -0400

    Created node.js web server following Express Crash Course video

---
+++ public/index.js
@@ -0,0 +1,31 @@
+const presets = {
+    round1: [
+        { chemical: '3-Way', rate: 1.0 },
+        { chemical: 'Triclopyr', rate: 0.25 },
+        { chemical: 'ChemStick', rate: 8.0 }
+    ],
+    round2: [
+        { chemical: '3-Way', rate: 1.0 },
+        { chemical: 'Triclopyr', rate: 0.25 },
+        { chemical: 'Dimension', rate: 0.4 },
+        { chemical: 'ChemStick', rate: 8.0 }
+    ],
+    round3: [
+        { chemical: 'Spartan/ 3-Way', rate: 1.0 },
+        { chemical: 'ChemStick', rate: 8.0 }
+    ],
+    round4: [
+        { chemical: 'All-Star', rate: 3.0 },
+        { chemical: 'ChemStick', rate: 8.0 }
+    ],
+    round5: [
+        { chemical: '3-Way', rate: 1.0 },
+        { chemical: 'Triclopyr', rate: 0.25 },
+        { chemical: 'ChemStick', rate: 8.0 }
+    ],
+    round6: [
+        { chemical: '3-Way', rate: 1.0 },
+        { chemical: 'Triclopyr', rate: 0.25 },
+        { chemical: 'ChemStick', rate: 8.0 }
+    ],
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
