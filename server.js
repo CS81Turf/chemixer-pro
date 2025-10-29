27c2410231d270b1a0ad5eb244fbd3bb16c74d68
Chris <chrishabich3@gmail.com>
2025-10-29 11:04:40 -0400

    Created static files and .env file

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'about.html'));
+// });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
