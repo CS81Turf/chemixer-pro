e05855e016de8b1a695ba0f3eed5311e28069034
Chris <chrishabich3@gmail.com>
2025-12-10 15:36:12 -0500

    Added error handling for /api/presets call

--- public/js/presets.js
+++ public/js/presets.js
@@ -1,11 +1,14 @@
-let response = await fetch('/api/presets')
-
 //check resonse ok
+try {
+  let response = await fetch("/api/presets");
+  if (!response.ok) {
+    throw new Error("Failed to load presets");
+  }
 
-
-
-
-let responseData = await response.json()
-let presets = responseData;
+  let responseData = await response.json();
+  let presets = responseData;
+} catch (err) {
+  console.error("Error loading presets.");
+}
 
 export { presets };
